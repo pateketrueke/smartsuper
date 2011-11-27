4eb11846e7e3b452f7861ce186ae08d84518873e
Alvaro Cabrera <pateketrueke@gmail.com>
2011-11-27 07:46:19 -0600

    configuracion para prod

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
 
-$config['rewrite'] = strpos(value($_SERVER, 'HTTP_HOST'), '.com') ? 1 : 0;
+$config['rewrite'] = 0;
 $config['database'] = 'sqlite:'.getcwd().DS.'database'.DS.'db.sqlite';
+
+
+if (strpos(value($_SERVER, 'HTTP_HOST'), '.com')) {
+  $config['rewrite'] = 1;
+  $config['database'] = str_replace('postgres:', 'pgsql:', getenv('SHARED_DATABASE_URL'));
+}
